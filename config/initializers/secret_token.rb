e1d5d43077b61c334a6930826419a8dd21021f42
Luis G Romero <luisgromero@live.com>
2013-10-22 22:27:29 -0700

    Initial project 'SimpleWebsite' setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleWebsite::Application.config.secret_token = '1761322ffbd5adb955866ecf9e3a5132b965e87f15d33be72808ce1fd869409e615c1ade029880cdb847b1a2782bad37bbc7876f80b96a8a9a3c8c0723d397de'
